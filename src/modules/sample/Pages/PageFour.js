3eb68f33e617c6f14714d1d0689679fac3df9970
DeekshitaPalparty <deekshitapalparty9@gmail.com>
2020-10-26 12:22:01 +0530

    audit page added

--- src/modules/sample/Pages/PageFour.js
+++ src/modules/sample/Pages/PageFour.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import FilterListIcon from '@material-ui/icons/FilterList';
 import SimpleSelect from '../Pages/cards/UI/dropDown/dropDown';
 import TimeDrop from '../Pages/cards/UI/dropDown/dropdown_time';
@@ -8,78 +8,56 @@ import TimeDrop from '../Pages/cards/UI/dropDown/dropdown_time';
 //import Badge from '../../../@crema/core/Badge/index';
 import CalcCard from './cards/calc_card';
 import CalcCard2 from './cards/calc_card2';
-import  SimpleTabs from './cards/UI/ui_pagethree/tab';
-import CustomizedTables from './cards/UI/ui_pagefour/table_datasource';
+import SimpleTabs from './cards/UI/ui_pagethree/tab';
+import CustomizedTables from './cards/UI/ui_pagefour/table';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import AppAnimate from '../../../@crema/core/AppAnimate';
 import Card from '@material-ui/core/Card';
-import { Grid } from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 import SimplePopover from './cards/UI/popover';
 const useStyles = makeStyles({
   root1: {
     width: '100%',
-    height:1700,
-    backgroundColor:'rgb(246,248,249)'
-   
-   
+    height: 1700,
+    backgroundColor: 'rgb(246,248,249)',
   },
-  flexcontainer:{
-
-    display: "flex"
+  flexcontainer: {
+    display: 'flex',
   },
-  flexchild:{
-    flex:0.6,
-    border:"none"
-
+  flexchild: {
+    flex: 0.6,
+    border: 'none',
   },
-  grid:{
-    paddingLeft:'40px',
-    paddingRight:'40px',
-    paddingTop:'40px'
-    
+  grid: {
+    paddingLeft: '40px',
+    paddingRight: '40px',
+    paddingTop: '40px',
   },
-  tab:{
-    marginTop:100
-  }
-
+  tab: {
+    marginTop: 100,
+  },
 });
-const PageFour= () => {
+const PageFour = () => {
   const classes = useStyles();
   return (
-   
     <AppAnimate animation='transition.slideUpIn' delay={200}>
       <Box>
-        <Box component='h4'  fontSize={20}>
-        
-         <Card className={classes.root1} >
-           <CardHeader>
-             <SimplePopover/>
-           </CardHeader>
-           <CardContent>
-           <Grid container spacing={4} className={classes.grid} >
-         
-           
-         <CustomizedTables />
-           
-           
-         </Grid>
-       
-            
-           </CardContent>
-         
-
-      
-
-        
-         </Card>
-        
+        <Box component='h4' fontSize={20}>
+          <Card className={classes.root1}>
+            <CardHeader>
+              <SimplePopover />
+            </CardHeader>
+            <CardContent>
+              <Grid container spacing={4} className={classes.grid}>
+                <CustomizedTables />
+              </Grid>
+            </CardContent>
+          </Card>
         </Box>
-       
       </Box>
     </AppAnimate>
   );
 };
 
 export default PageFour;
-
